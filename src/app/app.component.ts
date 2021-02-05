6cae11ed6d2f7a87508bbb3fcc31ef2b5791c8da
sandrine <sandrinezocca@gmail.com>
2021-02-05 16:20:02 +0100

    the Summary component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { StocksService, StockInterface } from './services/stocks.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'stocks';
+  stocks: Array<StockInterface>;
+
+  constructor(service: StocksService) {
+    service.load(['AMZN']).subscribe((stocks) => {
+      this.stocks = stocks;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 import { StocksService } from './services/stocks.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
